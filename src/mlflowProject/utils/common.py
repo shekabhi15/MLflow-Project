11c1814bf0250214ad904df499d58f9e812f34b3
shekabhi15 <abhi1538999@gmail.com>
2023-08-04 16:10:46 +0530

    data ingestion

--- src/mlflowProject/utils/common.py
+++ src/mlflowProject/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-from mlflowProject import logger
+from src.mlflowProject import logger
 import json
 import joblib
 from ensure import ensure_annotations
